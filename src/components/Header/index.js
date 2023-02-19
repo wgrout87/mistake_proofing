25e268b9acad57da81e4cf41b11b46980b427959
Will G <wgrout87@gmail.com>
2023-02-19 09:31:33 -0700

    header

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="d-flex justify-content-between">
+            <h1>John Grout's Mistake-Proofing Center</h1>
+        </header>
+    )
+}
+
+export default Header;
