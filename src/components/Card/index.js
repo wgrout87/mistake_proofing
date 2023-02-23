c3ae06b874ad5e11c628f0cbf19a2b4ba87560c5
Will G <wgrout87@gmail.com>
2023-02-22 21:54:55 -0700

    modified card styles

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,11 +3,11 @@ import Card from 'react-bootstrap/Card';
 
 const ExampleCard = ({ img, title, text }) => {
     return (
-        <Card style={{ width: '18rem' }} className="m-2">
-            <Card.Img variant="top" src={`./images/${img}.jpg`} />
-            <Card.Body>
-                <Card.Title>{title}</Card.Title>
-                <Card.Text>
+        <Card style={{ width: '18rem' }} className="m-2 px-0">
+            <Card.Img className="mt-2" variant="top" src={`./images/${img}.jpg`} />
+            <Card.Body className="pt-0 px-0">
+                <Card.Title className="bg-dark text-light p-1">{title}</Card.Title>
+                <Card.Text className="px-3">
                     {text}
                 </Card.Text>
             </Card.Body>
