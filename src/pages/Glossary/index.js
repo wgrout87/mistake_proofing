bff5b2328de605471e4d21ddab7c2f768d59b1e2
Will G <wgrout87@gmail.com>
2023-02-23 21:13:08 -0700

    glossary maps over entries array

--- src/pages/Glossary/index.js
+++ src/pages/Glossary/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import GlossaryEntry from "../../components/GlossaryEntry";
+import entries from "./entries"
 
 const Glossary = () => {
     return (
         <main className="d-flex flex-column">
             <h1>Glossary</h1>
             <div className="container">
-                <GlossaryEntry
-                    word={"ACTIVE ERROR"}
-                    definition={"An active error has immediate consequences. Compare to latent errors."}
-                />
+                {entries.map((entry, index) => (
+                    <GlossaryEntry
+                        key={`entry${index}`}
+                        word={entry.word}
+                        definition={entry.definition}
+                    />
+                ))}
             </div>
         </main>
     );
