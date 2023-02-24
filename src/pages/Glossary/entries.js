8e5d938f69407925627e0de08a90b35b3c41c6c8
Will G <wgrout87@gmail.com>
2023-02-23 21:10:34 -0700

    added entries array for glossary

---
+++ src/pages/Glossary/entries.js
@@ -0,0 +1,12 @@
+var entries = [
+    {
+        word: "ACTIVE ERROR",
+        definition: "An active error has immediate consequences. Compare to latent errors."
+    },
+    {
+        word: "ALTOGETHER METHODS",
+        definition: "Method used to insure that all parts of an operation have been completed. See fixed value method."
+    },
+]
+
+export default entries;
