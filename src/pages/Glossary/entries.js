59110eb1b7d1194f44c33541356b91f9f0d905eb
Will G <wgrout87@gmail.com>
2023-02-24 23:07:24 -0700

    reformatted glossary entries

--- src/pages/Glossary/entries.js
+++ src/pages/Glossary/entries.js
@@ -1,11 +1,16 @@
 var entries = [
     {
-        word: "ACTIVE ERROR",
-        definition: "An active error has immediate consequences. Compare to latent errors."
-    },
-    {
-        word: "ALTOGETHER METHODS",
-        definition: "Method used to insure that all parts of an operation have been completed. See fixed value method."
+        letter: "A",
+        words: [
+            {
+                word: "ACTIVE ERROR",
+                definition: "An active error has immediate consequences. Compare to latent errors."
+            },
+            {
+                word: "ALTOGETHER METHODS",
+                definition: "Method used to insure that all parts of an operation have been completed. See fixed value method."
+            },
+        ]
     },
 ]
 
