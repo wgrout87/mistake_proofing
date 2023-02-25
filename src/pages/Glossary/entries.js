d9f23bda3becf1fb5a68af4129a4b19a8c8fdd57
Will G <wgrout87@gmail.com>
2023-02-25 08:43:36 -0700

    added all A entries

--- src/pages/Glossary/entries.js
+++ src/pages/Glossary/entries.js
@@ -18,6 +18,42 @@ var entries = [
                 word: "action slips",
                 definition: "An error where the intent was correct but action did not occur as intended."
             },
+            {
+                word: "action step methods",
+                definition: "setting function discussed by Monden. It requires workers to perform a step that is not part of the operations on the product, like insuring the worker inserts their hand in the correct parts bin. See also motion-step method."
+            },
+            {
+                word: "activation-trigger-schemata system",
+                definition: "actions which are commonly performed over time will become established as unconscious routines called schemas (or schemata, both are plural of schema). When the conscious mind intends to perform some action a group of relevant schemas are activated. Activated schema stand ready, waiting for specific triggers or cues from the environment in order to actually execute the schema. Errors result from unintentional activation of inappropriate schemas, loss of activation through interruption or forgotten intention, or faulty triggering of active schemas -- either false triggering by a similar cue, or failure to trigger. The more highly active a schema is, the easier it is to trigger it with similar cues Interruptions and interference by the conscious mind also play a major role in faulty triggering."
+            },
+            {
+                word: "adjustment error",
+                definition: "number 8 of Nikkan Kogyo Shimbun (Factory Magazine) top 10 sources of defects"
+            },
+            {
+                word: "alarms",
+                definition: "one of Shingo's five basic classes of poka yoke devices. Used to cue workers to take action when an error or defect has occurred. See warning methods"
+            },
+            {
+                word: "associative activation errors",
+                definition: "a type of slip identified by Norman that is caused by an internal association of two activities. For example, calling children by their siblings' name."
+            },
+            {
+                word: "ATS",
+                definition: "see activation-trigger-schemata"
+            },
+            {
+                word: "attribute control charts",
+                definition: "a control chart used to plot data that is not based on actual numerical measurements. Attribute control charts include proportion defective (p) charts, number defective (np) charts, count of defects (c) charts, average number of defects (u) charts."
+            },
+            {
+                word: "automation-induced complacency",
+                definition: "the inattention that results from a human monitoring reliable automated equipment. As the perceived need for active intervention declines, the mind wanders. The ability to assess the state of the process and appropriately intervene is reduced."
+            },
+            {
+                word: "autonomation",
+                definition: "the automatic control of defects. It includes the practice of stopping machines when some deviation or variance is detected. The detection and stoppage can be effected by either a worker or a machine. Detection schemes include contact, altogether, and action step methods. Mistake-proofing as a quality control technique is a subset of autonomation. Autonomation can also be employed for other, non-quality, purposes."
+            },
         ]
     },
     {
